e705160fdde227893512b0448d23a4affb1205e7
Ahamed Kaleemullah <ahamed.kaleemullah@gmail.com>
2024-08-05 20:01:22 +0000

    table

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
-function (Controller) {
+function (Controller,JSONModel) {
     "use strict";
 
     return Controller.extend("zpractise.controller.View1", {
         onInit: function () {
+var oNameModel = new JSONModel();
+var names = {
+    "firstName" : "Ahamed",
+    "lastName" : "Kaleemullah"
+}
+
+oNameModel.setData(names);
+debugger;
+var fName = oNameModel.getProperty("/firstName");
+var lName = oNameModel.getProperty("/lastName");
+
+this.getView().byId("idFirstName").setValue(fName);
+this.getView().byId("idLastName").setValue(lName);
+this.getView().byId("idFullName").setValue(fName + " " + lName);
 
         },
         onNameChange : function(){
@@ -14,6 +29,12 @@ function (Controller) {
             var fullName = firstName + " " + lastName;
 
             this.getView().byId("idFullName").setValue(fullName);
+        },
+        onListPress:function(oEvent){
+            debugger;
+            var oContextPath = oEvent.getSource().getBindingContextPath();
+            this.getView().bindElement(oContextPath);
+            console.log(oEvent);
         }
     });
 });
